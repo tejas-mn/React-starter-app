324ac57b286bac310b445b74f364c17fe1c6308d
tejas-mn <tejasmn2020@gmail.com>
2021-10-15 14:30:05 +0530

    Initial Commit

---
+++ src/Button.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Button.css'
+
+function Button(){
+    return(
+        <>
+            <center><button className="btn">Welcome</button></center>
+        </>
+        
+    );
+}
+
+export default Button;
